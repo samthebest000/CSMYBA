034ff9a8da9c3da041c6dcaf9f1a31be93931219
samthebest000 <samoaprem60@gmail.com>
2023-03-23 14:20:59 +0300

    "Working MongoDB Realm with cars example"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:realm/realm.dart';
+import 'car.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +33,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<List<Car>> _getAllCars() async {
+    var app = App(AppConfiguration("yba-lmcvl"));
+    var loggedInUser = await app.logIn(Credentials.anonymous());
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+    final config = Configuration.flexibleSync(loggedInUser, [Car.schema]);
+    final realm = Realm(config);
+
+    realm.subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.add(realm.query<Car>(r'model == $0', ["Model S"]));
     });
+    await realm.subscriptions.waitForSynchronization();
+
+    final cars = realm.all<Car>().toList();
+
+    return cars;
+  }
+
+  Future<void> _deleteAllCars() async {
+    var app = App(AppConfiguration("yba-lmcvl"));
+    var loggedInUser = await app.logIn(Credentials.anonymous());
+
+    final config = Configuration.flexibleSync(loggedInUser, [Car.schema]);
+    final realm = Realm(config);
+
+    realm.write(() {
+      realm.deleteAll<Car>();
+    });
+
+    setState(() {});
   }
 
   @override
@@ -43,24 +70,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: FutureBuilder<List<Car>>(
+        future: _getAllCars(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.data!.isEmpty) {
+            return const Center(child: Text("No cars found!"));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              var data = snapshot.data!;
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("ID: ${data[index].id}"),
+                    Text("Make: ${data[index].make}"),
+                    Text("model: ${data[index].model}"),
+                    Text("miles: ${data[index].miles}"),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _deleteAllCars,
+        child: const Icon(Icons.remove),
       ),
     );
   }
